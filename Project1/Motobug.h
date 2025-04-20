c21ae621ef7c53db7b4bec916164fba111c133ee
Muhammad Hamza <hxmza.04072005@gmail.com>
2025-04-20 18:22:53 +0500

    Adding Motobug

---
+++ Project1/Motobug.h
@@ -0,0 +1,76 @@
+#pragma once
+#include "Src.cpp"
+
+class Motobug {
+
+private:
+
+	float x, y;
+	int hp;
+	Texture texture;
+	Sprite sprite;
+	bool isAlive;
+
+
+public:
+
+	Motobug() {
+
+		this->hp = 2;
+		isAlive = true;
+		this->x = 250;
+		this->y = 150;
+		texture.loadFromFile("Sprites/PC Computer - Sonic Mania - Badniks Plus.png");
+		sprite.setTexture(texture);
+		sprite.setTextureRect(IntRect(0, 0, 32, 32));
+		sprite.setPosition(x, y);
+		sprite.setScale(2.0f, 2.0f);
+
+	}
+
+	int getX() {
+
+		return x;
+	}
+
+	int getY() {
+
+		return y;
+	}
+
+	int getHp() {
+
+		return hp;
+	}
+
+
+	void movement(float player_x, float player_y) {
+
+		if (player_x > x) {
+
+			x += 2.0f;
+		}
+
+		else if (player_x < x) {
+
+			x -= 2.0f;
+		}
+
+		sprite.setPosition(x, y);
+
+	}
+
+	void draw(RenderWindow& window) {
+
+		if (isAlive) {
+
+			window.draw(sprite);
+		}
+
+		else {
+
+			sprite.setPosition(999, 999);
+		}
+		
+	}
+};
